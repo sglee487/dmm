09dcaee363840f161badf3f9f4b4e3c72f760502
sglee487 <sglee487@gmail.com>
2019-05-16 19:29:59 +0900

    modify reference

--- forMidi/Midi-main.py
+++ forMidi/Midi-main.py
@@ -1,5 +1,4 @@
-# http://aqibsaeed.github.io/2016-09-03-urban-sound-classification-part-1/
-# http://aqibsaeed.github.io/2016-09-24-urban-sound-classification-part-2/
+# https://github.com/sglee487/dmm
 
 import os
 import numpy as np
